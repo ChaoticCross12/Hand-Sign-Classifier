35166b73bf6469ff3be7b8280d28603631cd7db6
Meah Tahmeed Ahmed <tahmeed121212@gmail.com>
2020-11-21 05:19:13 +0600

    Fixed importS

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense

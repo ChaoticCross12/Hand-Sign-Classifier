8a3d709f8103f23640fda4feef4d1261057d8352
Meah Tahmeed Ahmed <tahmeed121212@gmail.com>
2021-01-10 03:36:30 +0600

    testing predict

--- model.py
+++ model.py
@@ -2,13 +2,17 @@
 
 import keras
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization
+from keras.preprocessing import image
+from keras.models import Sequential, load_model
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Activation, Dropout, Flatten, Dense
+from keras.layers import BatchNormalization
 from keras import backend as K
 import numpy as np
 from keras.preprocessing import image
 from keras.callbacks import History
+
+import cv2
 
 import matplotlib.pyplot as plt
 
@@ -61,7 +65,9 @@ validation_generator = datagen.flow_from_directory(
 #THE MODEL
 
 model = keras.Sequential()
+
 # Set image shape: input_shape=(32, 32, 3)
+
 model.add(Conv2D(32, kernel_size = (3, 3), input_shape=inputShape))
 model.add(Activation('relu'))
 model.add(BatchNormalization())
@@ -105,42 +111,53 @@ model.add(Dense(3, activation='softmax'))
 
 # Training
 
-rms = keras.optimizers.RMSprop(learning_rate= 0.5, rho = 0.9)
+rms = keras.optimizers.RMSprop(learning_rate= 0.01, rho = 0.9)
+
+
+# Loading the trained model
+
+
+
 
 model.compile(loss = 'categorical_crossentropy',
               optimizer = 'adam',
               metrics = ['categorical_accuracy'])
 
+#history = History()
+#
+#
+## fit   wda
+##batch size = 2
+#
+#model.fit_generator(
+#    train_generator,
+#    steps_per_epoch = 821,
+#    epochs = 20, callbacks = [history],
+#    validation_data = validation_generator,
+#    validation_steps = 656
+#)
+#
+## Saving Model
+#model.save_weights("modelNew.h5")
+#print("Saved model to disk")
+#
+#
+#plt.plot(history.history['categorical_accuracy'])
+#plt.plot(history.history['val_categorical_accuracy'])
+#plt.title('model accuracy')
+#plt.ylabel('accuracy')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'test'], loc='upper left')
+#plt.show()
 
 
-history = History()
+# Loading
 
-# fit   wda
-#batch size = 2
+model.load_weights("modelNew.h5")
 
-model.fit_generator(
-    train_generator,
-    steps_per_epoch = 821,
-    epochs = 3, callbacks = [history],
-    validation_data = validation_generator,
-    validation_steps = 656
-)
+img_pred = image.load_img('predTestR.png', target_size = (img_width, img_height))
+img_pred = image.img_to_array(img_pred)
+img_pred = np.expand_dims(img_pred, axis = 0)
 
-# Saving Model
-model.save_weights("model.h5")
-print("Saved model to disk")
-
-
-
-
-model.summary()
-
-print(history.history.keys())
-
-plt.plot(history.history['categorical_accuracy'])
-plt.plot(history.history['val_categorical_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
+rstl = model.predict(img_pred)
+print(rstl)
